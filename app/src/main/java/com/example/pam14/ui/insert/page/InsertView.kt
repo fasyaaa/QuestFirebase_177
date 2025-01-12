74e729655cf3a0e9b8a3ff4979469f8aebd32b1d
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-01-13 01:26:52 +0700

    ADD - Implement Scaffold layout with top bar and body for InsertMhsView

--- app/src/main/java/com/example/pam14/ui/insert/page/InsertView.kt
+++ app/src/main/java/com/example/pam14/ui/insert/page/InsertView.kt
@@ -1,11 +1,20 @@
 package com.example.pam14.ui.insert.page
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pam14.ui.PenyediaViewModel
 import com.example.pam14.ui.insert.viewmodel.FormState
@@ -43,6 +52,41 @@ fun InsertMhsView(
                 }
             }
             else -> Unit
+        }
+    }
+
+    Scaffold (
+        modifier = modifier,
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
+        topBar = {
+            TopAppBar(
+                title = {Text("Tambah Mahasiswa")},
+                navigationIcon = {
+                    Button(onClick = onBack) {
+                        Text("Back")
+                    }
+                }
+            )
+        }
+    ){
+        padding ->
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .padding(padding)
+            .padding(16.dp)
+        ) {
+            InsertBodyMhs(
+                uiState = uiEvent,
+                homeUiState = uiState,
+                onValueChange = { updateEvent ->
+                    viewModel.updateState(updateEvent)
+                },
+                onClick = {
+                    if (viewModel.validateFields()) {
+                        viewModel.insertMhs()
+                    }
+                }
+            )
         }
     }
 }
