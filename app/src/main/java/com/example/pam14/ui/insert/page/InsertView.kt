c74da9aa0e0779a9e199a7570f596a678bf5e6ef
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-01-13 00:42:37 +0700

    ADD - Handle UI state changes with LaunchedEffect in InsertMhsView

--- app/src/main/java/com/example/pam14/ui/insert/page/InsertView.kt
+++ app/src/main/java/com/example/pam14/ui/insert/page/InsertView.kt
@@ -2,12 +2,16 @@ package com.example.pam14.ui.insert.page
 
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pam14.ui.PenyediaViewModel
+import com.example.pam14.ui.insert.viewmodel.FormState
 import com.example.pam14.ui.insert.viewmodel.InsertViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @Composable
 fun InsertMhsView(
@@ -20,4 +24,25 @@ fun InsertMhsView(
     val uiEvent = viewModel.uiEvent
     val snackbarHostState = remember {SnackbarHostState()}
     val coroutineScope = rememberCoroutineScope()
+
+    LaunchedEffect(uiState) {
+        when (uiState){
+            is FormState.Success -> {
+                println("InsertMhsView: uiState is FormState.Success, navigate to home" + uiState.message)
+                coroutineScope.launch {
+                    snackbarHostState.showSnackbar(uiState.message) // Tampilkan Snackbar
+                }
+                delay(700)
+                // Navigasi langsung
+                onNavigate()
+                viewModel.resetSnackBarMessage() // Reset snackbar state
+            }
+            is FormState.Error -> {
+                coroutineScope.launch {
+                    snackbarHostState.showSnackbar(uiState.message)
+                }
+            }
+            else -> Unit
+        }
+    }
 }
