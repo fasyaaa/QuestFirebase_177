f70e478860db24d779fdc7f9783322640aad7aa6
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-01-12 21:04:23 +0700

    ADD - Create InsertUiState class to manage Mahasiswa event and form validation state

--- app/src/main/java/com/example/pam14/ui/home/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/pam14/ui/home/viewmodel/InsertViewModel.kt
@@ -2,6 +2,11 @@ package com.example.pam14.ui.home.viewmodel
 
 import com.example.pam14.model.Mahasiswa
 
+data class InsertUiState(
+    val insertUiEvent: MahasiswaEvent = MahasiswaEvent(),
+    val isEntryValid: FormErrorState = FormErrorState()
+)
+
 data class FormErrorState(
     val nim: String? = null,
     val nama: String? = null,
