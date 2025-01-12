4479e60a8bfa798535434e0f3594bae6dfdb3e0e
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-01-13 00:36:15 +0700

    ADD - InsertMhsView Composable to handle Mahasiswa insertion UI

--- app/src/main/java/com/example/pam14/ui/insert/page/InsertView.kt
+++ app/src/main/java/com/example/pam14/ui/insert/page/InsertView.kt
@@ -1,2 +1,23 @@
 package com.example.pam14.ui.insert.page
 
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.pam14.ui.PenyediaViewModel
+import com.example.pam14.ui.insert.viewmodel.InsertViewModel
+
+@Composable
+fun InsertMhsView(
+    onBack: () -> Unit,
+    onNavigate: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val uiState = viewModel.uiState //
+    val uiEvent = viewModel.uiEvent
+    val snackbarHostState = remember {SnackbarHostState()}
+    val coroutineScope = rememberCoroutineScope()
+}
