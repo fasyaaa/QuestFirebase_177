203591d43cf4624805fe58bf850a0b2890fef096
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-01-12 16:41:27 +0700

    ADD - Create data class MahasiswaEvent to represent Mahasiswa properties

--- app/src/main/java/com/example/pam14/ui/home/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/pam14/ui/home/viewmodel/InsertViewModel.kt
@@ -1,2 +1,10 @@
 package com.example.pam14.ui.home.viewmodel
 
+data class MahasiswaEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val jenisKelamin: String = "",
+    val alamat: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
