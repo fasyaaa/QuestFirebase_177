9c39c7aa5f950bb800b9d0f340691ac1ed85a409
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-01-13 13:33:41 +0700

    ADD - DestinasiNavigasi selesai

--- app/src/main/java/com/example/pam14/ui/navigation/DestinasiNavigasi.kt
+++ app/src/main/java/com/example/pam14/ui/navigation/DestinasiNavigasi.kt
@@ -1,2 +1,16 @@
 package com.example.pam14.ui.navigation
 
+interface DestinasiNavigasi {
+    val route: String
+    val titleRes: String
+}
+
+object DestinasiHome : DestinasiNavigasi{
+    override val route: String = "home"
+    override val titleRes: String = "Home"
+}
+
+object DestinasiInsert : DestinasiNavigasi{
+    override val route: String = "insert"
+    override val titleRes: String = "Insert"
+}
