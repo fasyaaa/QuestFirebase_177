46f85b450e155ea7029b0bff5c5147ad040a9fea
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-01-15 16:31:34 +0700

    ADD - scroll

--- app/src/main/java/com/example/pam14/ui/insert/page/InsertView.kt
+++ app/src/main/java/com/example/pam14/ui/insert/page/InsertView.kt
@@ -2,6 +2,7 @@ package com.example.pam14.ui.insert.page
 
 import android.graphics.Paint.Align
 import android.os.Build
+import android.widget.ScrollView
 import androidx.annotation.RequiresExtension
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -12,7 +13,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -112,11 +115,13 @@ fun InsertMhsView(
             )
         }
     }
+
 }
 
 @Composable
 fun InsertBodyMhs(
-    modifier: Modifier = Modifier,
+    modifier: Modifier = Modifier
+        .verticalScroll(rememberScrollState()),
     onValueChange: (MahasiswaEvent) -> Unit,
     uiState: InsertUiState,
     onClick: () -> Unit,
@@ -162,6 +167,8 @@ fun FormMahasiswa(
 ) {
     val jenisKelamin = listOf("Laki-Laki", "Perempuan")
     val kelas = listOf("A", "B", "C", "D", "E")
+
+
 
     Column(
         modifier = modifier.fillMaxWidth()
