707597e0c0e615392765c134699f130ed4f862ab
fasyaaa <muhammad.a.f.2210@gmail.com>
2025-01-15 15:47:19 +0700

    ADD - DestinasiDetail

--- app/src/main/java/com/example/pam14/ui/navigation/DestinasiNavigasi.kt
+++ app/src/main/java/com/example/pam14/ui/navigation/DestinasiNavigasi.kt
@@ -14,3 +14,9 @@ object DestinasiInsert : DestinasiNavigasi{
     override val route: String = "insert"
     override val titleRes: String = "Insert"
 }
+
+object DestinasiDetail : DestinasiNavigasi{
+    override val route: String = "detail"
+    override val titleRes: String = "Detail"
+    const val  nim = "nim"
+}
